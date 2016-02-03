5d80bf2fc84a508b816b17c39be24953e371e0d2
Chaoran Cheng <chc286@ucsd.edu>
2016-02-03 07:47:48 -0800

    layout for homepage, create event page

--- MeetUp/app/src/main/java/com/ucsd/meetup/LoginActivity.java
+++ MeetUp/app/src/main/java/com/ucsd/meetup/LoginActivity.java
@@ -3,6 +3,7 @@ package com.ucsd.meetup;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -116,6 +117,11 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         mProgressView = findViewById(R.id.login_progress);
     }
 
+    public void goToMainActivity(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
     private void populateAutoComplete() {
         if (!mayRequestContacts()) {
             return;
@@ -209,6 +215,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(true);
             mAuthTask = new UserLoginTask(email, password);
             mAuthTask.execute((Void) null);
+
         }
     }
 
